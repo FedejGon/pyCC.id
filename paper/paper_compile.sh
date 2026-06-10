f1e8d4f82d19fc2534a45f71f5279f4e2d087820
fede <fede.jgon@gmail.com>
2026-06-10 16:05:24 -0300

    Open Journals PDF Generator file .github/workflows/draft-pdf.yml

--- paper/paper_compile.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-#pandoc paper.md -o paper.pdf
-#pandoc paper.md --biblatex --bibliography=paper.bib -o paper.pdf
-#pandoc paper.md --biblatex --pdf-engine=latexmk -o paper.pdf
-
-pandoc paper.md --biblatex --pdf-engine=latexmk  --include-in-header=biblatex-config.tex -V geometry:margin=1.5cm  -o paper.pdf
